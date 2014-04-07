a567f308ae3bf1b09c8f9c459e08d04824d2a4ef
Dori Ding <sghcel.ok@163.com>
2014-04-07 20:58:30 +0800

    Initial commit

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ var config = {
   db: 'mongodb://127.0.0.1/node_club_dev',
   session_secret: 'node_club',
   auth_cookie_name: 'node_club',
-  port: 3000,
+  port: 3001,
 
   // 话题列表显示的话题数量
   list_topic_count: 20,
